8f9502d428aa54b01bb1491d630189d32a1f44a0
Sota529 <za446131@gmail.com>
2021-02-11 02:55:43 +0900

    go mod とdatabaseとgoの接続

--- main.go
+++ main.go
@@ -1,14 +1,37 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	// "time"
+
+	// "github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+)
 
 func main() {
-    r := gin.Default()
-    r.GET("/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "id": "hoge",
-            "name":"hoge",
-        })
-    })
-    r.Run() 
+    fmt.Println("DB接続開始")
+
+    _, err := sqlConnect()
+    if err != nil {
+        panic(err.Error())
+    } else {
+        fmt.Println(err)
+        fmt.Println("DB接続成功")
+    }
 }
+
+// SQLConnect DB接続
+func sqlConnect() (database *gorm.DB, err error) {
+    DBMS := "mysql"
+    USER := "root"
+    PASS := "root"
+    PROTOCOL := "tcp(docker_MySQL:3306)"
+    DBNAME := "go_database"
+
+
+    CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
+    return gorm.Open(DBMS, CONNECT)
+}
+   
+
