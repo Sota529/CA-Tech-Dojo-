277b3ac7f218c59da6f4c11299900324702598a2
Sota529 <za446131@gmail.com>
2021-02-16 18:43:06 +0900

    initial commit

--- main.go
+++ main.go
@@ -2,36 +2,70 @@ package main
 
 import (
 	"fmt"
-	// "time"
+	"time"
 
-	// "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
 
+type User struct {
+    gorm.Model
+    Name string
+    Email string
+  }
+
 func main() {
-    fmt.Println("DB接続開始")
+  db := sqlConnect()
+  db.AutoMigrate(&User{})
+  defer db.Close()
 
-    _, err := sqlConnect()
-    if err != nil {
-        panic(err.Error())
-    } else {
-        fmt.Println(err)
-        fmt.Println("DB接続成功")
+  router := gin.Default()
+
+// GETメソッド
+  router.GET("/user/get", func(ctx *gin.Context){
+    fmt.Println("user got!")
+})
+// POSTメソッド
+  router.POST("/user/create", func(ctx *gin.Context){
+    fmt.Println("user created!")
+})
+
+  router.Run()
+}
+
+// mysql接続関数
+func sqlConnect() (database *gorm.DB) {
+  DBMS := "mysql"
+  USER := "go_test"
+  PASS := "password"
+  PROTOCOL := "tcp(db:3306)"
+  DBNAME := "go_database"
+
+  CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
+
+  count := 0
+  db, err := gorm.Open(DBMS, CONNECT)
+  
+//   30秒接続できなかったら接続失敗と出る処理
+  if err != nil {
+    for {
+      if err == nil {
+        fmt.Println("")
+        break
+      }
+      fmt.Print(".")
+      time.Sleep(time.Second)
+      count++
+      if count > 30 {
+        fmt.Println("")
+        fmt.Println("DB接続失敗")
+        panic(err)
+      }
+      db, err = gorm.Open(DBMS, CONNECT)
     }
+  }
+  fmt.Println("DB接続成功")
+
+  return db
 }
-
-// SQLConnect DB接続
-func sqlConnect() (database *gorm.DB, err error) {
-    DBMS := "mysql"
-    USER := "root"
-    PASS := "root"
-    PROTOCOL := "tcp(docker_MySQL:3306)"
-    DBNAME := "go_database"
-
-
-    CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
-    return gorm.Open(DBMS, CONNECT)
-}
-   
-
